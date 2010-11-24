ea58eb5c826f4527b2168cdbfc60de197f96bbf3
Jeff Watkins <jeff@metrocat.org>
2010-11-24 14:25:07 -0800

    Updated NIB generator. Also removed old distilery code.

--- lib/distilery.rb
+++
@@ -1,5 +0,0 @@
-script_dir=File.join(File.dirname(__FILE__), "distilery")
-
-require "#{script_dir}/nib-file.rb"
-require "#{script_dir}/bundle-task.rb"
-require "#{script_dir}/coherent-asset-filter.rb"
